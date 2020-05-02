1e9de1bb46e378e92f7f7e3704b9613d696450bf
Filipe Borges <filipebkc2209@gmail.com>
2020-05-02 18:30:10 -0300

    ControllerExceptionAdvice - Log exception implemented

--- src/main/java/br/com/cooperativa/assembleia/votacaoassembleiaapi/controller/ControllerExceptionAdvice.java
+++ src/main/java/br/com/cooperativa/assembleia/votacaoassembleiaapi/controller/ControllerExceptionAdvice.java
@@ -1,6 +1,8 @@
 package br.com.cooperativa.assembleia.votacaoassembleiaapi.controller;
 
 import br.com.cooperativa.assembleia.votacaoassembleiaapi.exception.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ControllerExceptionAdvice {
 
+    private final Logger logger = LoggerFactory.getLogger(ControllerExceptionAdvice.class);
+
     @ResponseBody
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String resourceNotFoundHandler(ResourceNotFoundException ex) {
-        // TODO: Log exception
+        logger.info("Resource Not Found", ex);
         return ex.getMessage();
     }
 
@@ -23,7 +27,7 @@ public class ControllerExceptionAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     String invalidParamHandler(MethodArgumentNotValidException ex) {
-        // TODO: Log exception
+        logger.info("Invalid Param On Endpoint Calls", ex);
         return ex.getMessage();
     }
 
@@ -31,7 +35,7 @@ public class ControllerExceptionAdvice {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     String internalServerError(Exception ex) {
-        // TODO: Log Exception
+        logger.error("Exception thrown", ex);
         return String.format("Internal Server Error - %s", ex.getMessage());
     }
 
